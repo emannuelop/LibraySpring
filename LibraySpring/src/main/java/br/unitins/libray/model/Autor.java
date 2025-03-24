ca5582d0f956b8f5b3e142e75402826347df784f
Danilo Da Silva Moreira Pires <danilolex5@gmail.com>
2025-03-24 10:07:52 -0300

    Model Livro e Autor

--- LibraySpring/src/main/java/br/unitins/libray/model/Autor.java
+++ LibraySpring/src/main/java/br/unitins/libray/model/Autor.java
@@ -1,5 +1,21 @@
 package br.unitins.libray.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
 public class Autor {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long idAutor;
+
+    @NotBlank(message = "O nome é obrigatório.")
+    String nome;
+
+    @NotBlank(message = "A nacionalidade é obrigatória.")
+    String nacionalidade;
 }
--- LibraySpring/src/main/java/br/unitins/libray/model/Livro.java
+++ LibraySpring/src/main/java/br/unitins/libray/model/Livro.java
@@ -1,5 +1,27 @@
 package br.unitins.libray.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
 public class Livro {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long idLivro;
+
+    @NotBlank(message = "O titulo é obrigatório.")
+    String titulo;
+
+    @NotBlank(message = "O autor do livro é obrigatório.")
+    Autor autor;
+
+    @NotBlank(message = "O Isbn é obrigatório.")
+    String isbn;
+
+    Boolean disponivel;
+
 }
