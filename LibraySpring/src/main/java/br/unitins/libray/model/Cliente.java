8481004bbc4415bfd2e39a6b41ee21ea891aa46c
Danilo Da Silva Moreira Pires <danilolex5@gmail.com>
2025-03-25 08:20:11 -0300

    Model Adm e Cliente

--- LibraySpring/src/main/java/br/unitins/libray/model/Cliente.java
+++ LibraySpring/src/main/java/br/unitins/libray/model/Cliente.java
@@ -1,5 +1,19 @@
 package br.unitins.libray.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
 public class Cliente {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long idCliente;
+
+    @NotBlank(message = "O nome é obrigatório.")
+    String nome;
+
 }
